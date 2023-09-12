817660295653d3d63c23ac589848656e32c4c352
rodrigo bueno <rodrigo.buenow@gmail.com>
2023-09-12 16:00:20 -0300

    adicionando a tabela ao localStorage e trazendo de volta para exibicao

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+
+    const arr = JSON.parse(localStorage.getItem("fornecedores")) || [];
+
+    const tabela = document.getElementById("tabelaFornecedores");
+
+    for (const fornecedor of arr) {
+        const novaLinha = tabela.insertRow(-1);
+
+        const colunaNome = novaLinha.insertCell(0);
+        colunaNome.innerHTML = fornecedor.nomeEmpresa;
+
+        const colunaProduto = novaLinha.insertCell(1);
+        colunaProduto.innerHTML = fornecedor.nomeContato;
+
+        const colunaProduto2 = novaLinha.insertCell(2);
+        colunaProduto2.innerHTML = fornecedor.nomePais;
+    }
+}
 function validarForm(nomeEmpresa, nomeContato, nomePais) {
     var validouCampo = true
     document.getElementById("nomeEmpresa").classList.remove("bordaCampoInvalido");
@@ -20,31 +39,35 @@ function validarForm(nomeEmpresa, nomeContato, nomePais) {
         }
              return validouCampo;
 }
-
-
 function cadastrarFornecedor(event) {
+    const arr = JSON.parse(localStorage.getItem("fornecedores")) || [];
+  
     event.preventDefault()
-    const nomeEmpresa = document.getElementById("nomeEmpresa").value;
-    const nomeContato = document.getElementById("nomeContato").value;
-    const nomePais = document.getElementById("nomePais").value;
 
-    if (validarForm(nomeEmpresa, nomeContato, nomePais)){
-        const tabela = document.getElementById("tabelaFornecedores");
-        const novaLinha = tabela.insertRow(-1);
+    const nomeEmpresa = document.getElementById("nomeEmpresa").value;
+    const nomeContato = document.getElementById("nomeContato").value;    
+    const nomePais = document.getElementById("nomePais").value; 
     
+    
+    if (validarForm(nomeEmpresa, nomeContato, nomePais)){
+        arr.push({nomeEmpresa, nomeContato, nomePais});
+        localStorage.fornecedores = JSON.stringify(arr);
+
+        const tabela = document.getElementById("tabelaFornecedores") ;
+        const novaLinha = tabela.insertRow(-1);
+
         const colunaNome = novaLinha.insertCell(0);
-        colunaNome.innerHTML = nomeEmpresa;
+        colunaNome.innerHTML = nomeEmpresa;        
     
         const colunaProduto = novaLinha.insertCell(1);
         colunaProduto.innerHTML = nomeContato;
     
         const colunaProduto2 = novaLinha.insertCell(2);
         colunaProduto2.innerHTML = nomePais;
+
     
         document.getElementById("nomeEmpresa").value=''
         document.getElementById("nomeContato").value=''
         document.getElementById("nomePais").value=''
-    }
-
-    
+    }  
 }
